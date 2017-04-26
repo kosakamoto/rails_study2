aca211d0c6daf7a068ce5f7e87dc107a0187a339
小坂元 俊輔 <kosakamoto0725@gmail.com>
2017-04-26 08:30:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsStudy2::Application.config.secret_key_base = '109e451ae4b246e73f9161faa3a4c4c3a1231d28a11e27c5af7b586d4d3bab1b9022f3c6547e70c9700ee9a565a1a3e2da8feb074ec34421494c48aa01d66c9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsStudy2::Application.config.session_store :cookie_store, key: '_rails_study2_session'
